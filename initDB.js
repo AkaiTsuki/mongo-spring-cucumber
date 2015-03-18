67240ab367aab61dc417742d39e1250573b3d1ba
Jiachi Liu <liu.jiach@husky.neu.edu>
2015-03-18 10:04:06 -0400

    Fixed mongoDb script  in Travis config file

---
+++ initDB.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jliu on 3/18/2015.
+ */
+
+db.customers.insert([
+    {firstName: 'John', lastName: 'Smith'},
+    {firstName: 'Joe', lastName: 'Smith'},
+    {firstName: 'Bob', lastName: 'Smith'},
+    {firstName: 'Ted', lastName: 'Smith'}
+]);
